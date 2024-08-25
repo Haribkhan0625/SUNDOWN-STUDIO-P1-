ebe588c749af8dbefca4b4b11193ea365d81612d
Abdul Wahab Khan Arib <haribkhan0625@gmail.com>
2024-08-25 14:10:26 -0700

    Some More Add On's

--- script.js
+++ script.js
@@ -6,17 +6,30 @@ const scroll = new LocomotiveScroll({
 var elemCont = document.querySelector("#elem-container");
 var fixedImage = document.querySelector("#fixed-image");
 
-elemCont.addEventListener("mouseenter", function(){
-    fixedImage.style.display = "block";
-});
-elemCont.addEventListener("mouseleave", function(){
-    fixedImage.style.display = "none";
-});
-
-var elems = document.querySelectorAll(".elem");
-elems.forEach(function(e){
-    e.addEventListener("mouseenter", function(){
-        var image = e.getAttribute("data-image");
-        fixedImage.style.backgroundImage = `url(${image})`;
+function page4Animation(){
+    elemCont.addEventListener("mouseenter", function(){
+        fixedImage.style.display = "block";
     });
-});
+    elemCont.addEventListener("mouseleave", function(){
+        fixedImage.style.display = "none";
+    });
+    
+    var elems = document.querySelectorAll(".elem");
+    elems.forEach(function(e){
+        e.addEventListener("mouseenter", function(){
+            var image = e.getAttribute("data-image");
+            fixedImage.style.backgroundImage = `url(${image})`;
+        });
+    });
+}
+
+function swiperAnimation(){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 50,
+      });
+    };
+
+    swiperAnimation();
+    page4Animation();
