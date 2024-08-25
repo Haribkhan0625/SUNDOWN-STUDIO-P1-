da47840b58130e238c64ab23830efb6348d8f0df
Abdul Wahab Khan Arib <haribkhan0625@gmail.com>
2024-08-24 17:44:35 -0700

    Some More Add On's

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+var elemCont = document.querySelector("#elem-container");
+var fixedImage = document.querySelector("#fixed-image");
+
+elemCont.addEventListener("mouseenter", function(){
+    fixedImage.style.display = "block";
+});
+elemCont.addEventListener("mouseleave", function(){
+    fixedImage.style.display = "none";
+});
+
+var elems = document.querySelectorAll(".elem");
+elems.forEach(function(e){
+    e.addEventListener("mouseenter", function(){
+        var image = e.getAttribute("data-image");
+        fixedImage.style.backgroundImage = `url(${image})`;
+    });
+});
